cd690d6b14f9c9460e418c1a0d76fde0e74bd77a
sierra <anon64273@gmail.com>
2024-11-11 17:00:57 +0100

    Update script.js

--- steam/script.js
+++ steam/script.js
@@ -2,6 +2,14 @@ async function loadGames() {
     const response = await fetch('list.json');
     const data = await response.json();
     const gamesList = document.getElementById('gamesList');
+    const audio = new Audio("click.wav");
+
+    function short(url) {
+        const regex = /images\/apps\/\d+\/([a-f0-9]{40})\.ico/;
+        const match = url.match(regex);
+        return match ? url.split('/')[4] : null;
+    }
+
     data.games.forEach(game => {
         const gameDiv = document.createElement('div');
         gameDiv.classList.add('game');
@@ -13,13 +21,14 @@ async function loadGames() {
         gameDiv.appendChild(icon);
         gameDiv.appendChild(gameName);
         gamesList.appendChild(gameDiv);
-    });
 
-    const audio = new Audio("click.wav");
-    document.querySelectorAll(".game").forEach((g) => {
-        g.title = g.textContent;
-        g.addEventListener("click", function() {
+        gameDiv.title = gameName.textContent;
+        gameDiv.addEventListener("click", function() {
             audio.play();
+            const appId = short(game.i);
+            if (appId) {
+                window.open(`https://store.steampowered.com/app/${appId}`, "_blank");
+            }
         });
     });
 }

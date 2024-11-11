343e54d4f7801701d0f26b5b459d8a06fbe64228
sierra <anon64273@gmail.com>
2024-11-11 16:17:27 +0100

    Update script.js

--- steam/script.js
+++ steam/script.js
@@ -1,13 +1,20 @@
 document.querySelectorAll(".game").forEach((g) => g.title=g.textContent)
-async function setGameIcons() {
-    document.querySelectorAll(".ico").forEach(async (icon) => {
-        const appId = icon.getAttribute("aid");
-        const response = await fetch(`https://store.steampowered.com/api/appdetails?appids=${appId}`);
-        const data = await response.json();
-        if (data[appId]?.success) {
-            icon.src = `https://cdn.cloudflare.steamstatic.com/steamcommunity/public/images/apps/${appId}/${data[appId].data.img_icon_url}.ico`;
-        }
+
+async function loadGames() {
+    const response = await fetch('list.json');
+    const data = await response.json();
+    const gamesList = document.getElementById('games-list');
+    data.games.forEach(game => {
+        const gameDiv = document.createElement('div');
+        gameDiv.classList.add('game');
+        const icon = document.createElement('img');
+        icon.classList.add('ico');
+        icon.src = game.i;
+        const gameName = document.createElement('span');
+        gameName.textContent = game.n;
+        gameDiv.appendChild(icon);
+        gameDiv.appendChild(gameName);
+        gamesList.appendChild(gameDiv);
     });
 }
-
-document.addEventListener("DOMContentLoaded", setGameIcons);
+document.addEventListener("DOMContentLoaded", loadGames);
